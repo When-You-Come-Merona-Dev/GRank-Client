aff322036bbac4e337361b0aeb04acbb37ca93e0
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-07-19 18:00:51 +0900

    feat(publish): header component

---
+++ components/Header/styled.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+
+
+export const Positioner = styled.div`
+    width : 100%;
+    height : 80px;
+`;
+
+export const HeaderWrapper = styled.div`
+    width : 100%;
+    height : 100%;
+    display : flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #63AD73;
+`;
+export const TitleWrapper = styled.div`
+    display : flex;
+    flex-direction: column;
+    strong {
+        font-size : 24px;
+        color: #fff;
+    }
+    span {
+        color : #fff;
+    }
+`;
+
+export const MenuWrapper = styled.div`
+    display : flex;
+    & > * {
+        cursor: pointer;
+        padding-left : 12px;
+        color : #fff;
+        font-weight : bold;
+    }
+`;
+
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,5 @@
 export { default as RankView } from "./RankView"
+export { default as Header } from "./Header"
 export { default as AddPersonModal } from "./AddPersonModal"
 export { default as AdminApplyView } from "./AdminApplyView"
 export { default as AdminView } from "./AdminView"

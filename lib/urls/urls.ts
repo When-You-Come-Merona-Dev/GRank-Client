3a4f8b20cc72beed431dacc687a39300cdc1cc5d
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-07-19 18:28:51 +0900

    feat(urls): add server api urls

--- lib/urls/urls.ts
+++ lib/urls/urls.ts
@@ -1,12 +1,19 @@
 const DEFAULT_URL = {
-    rank: "/github-user",
-    admin : "/admin"
+    rank: "/github-users",
+    admin: "/admin",
+    sns : '/sns'
 }
 
 export const RANK = {
     //get - 랭킹 불러오기, post - 랭킹에 인원 추가하기
     ranks: () => {
         return DEFAULT_URL.rank
+    },
+    addUser: () => {
+        return DEFAULT_URL.rank + `/${localStorage.getItem('username')}`
+    },
+    renew: () => {
+        return `${DEFAULT_URL.rank}/${localStorage.getItem('username')}/renew`
     },
     applyUser: (username : string) => {
         return `${DEFAULT_URL.rank}/${username}/approve`
@@ -16,5 +23,11 @@ export const RANK = {
 export const ADMIN = {
     login: () => {
         return `${DEFAULT_URL.admin}/login`
+    }
+}
+
+export const SNS = {
+    github: () => {
+        return `${DEFAULT_URL.sns}/github`
     }
 }

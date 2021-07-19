bd1c8825d9b8d8365bef81a360821b7cd80f305f
towel1017 <48384442+towel1017@users.noreply.github.com>
2021-07-19 18:19:44 +0900

    reafactor(style): RankItem change style

--- components/RankView/RankItem/styled.ts
+++ components/RankView/RankItem/styled.ts
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styled from "styled-components"
 
-export const Positioner = styled.div`
+export const Positioner = styled.div<{rank : number}>`
     width : 100%;
     display : flex;
     align-items: center;
@@ -12,7 +12,13 @@ export const Positioner = styled.div`
         padding : 0 14px;
         text-align: center;
         font-size: 15px;
+        color : #000;
+        font-weight: bold;
     }
+    & > span:nth-child(2) {
+        font-size : ${props => props.rank > 3 ? `20px` : `35px`}
+    }
+    
     border-bottom : 1px solid #c3c3c3;
 `;
 
@@ -20,23 +26,47 @@ export const ProfileImage = styled(Image)`
     border-radius: 100px;
 `;
 
+export const GithubIcon = styled(Image)`
+    cursor: pointer;
+    float: left;
+`;
+
 export const ProfileInfo = styled.div`
-        flex: 0.4;
+        flex: 0.22;
         display : flex;
         align-items: center;
-        div {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
+        justify-content: center;
+        & > div:last-child {
+            padding-left: 15px;
+            float: left;
         }
         span {
-            text-align: center;
-            padding : 3px 20px;
-            color : #c3c3c3;
+            width: 100%;
+            color : #CECECE;
         }
 `;
 
 export const IdText = styled.span`
-    color : #90AFFF !important;
+    color : #000 !important;
+    font-weight : bold;
     font-size : 18px;
+`;
+
+export const RankText = styled.span`
+    flex: 0.03;
+`;
+
+export const CommitCountText = styled.span`
+    flex:  0.2;
+`;
+
+export const GradeText = styled.span`
+    flex: 0.09;
+`;
+
+export const GroupListWrapper = styled.div`
+    flex: 0.49;
+    span {
+        color : #CECECE;
+    }
 `;
